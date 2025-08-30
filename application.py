d6f31f51214bde81ed1efebc1e91dbd907330700
kisel4363 <kisel4363@gmail.com>
2025-08-30 19:26:28 -0400

    + Implementing image and video embedding creation

--- application.py
+++ application.py
@@ -9,25 +9,23 @@ CORS(application )
 
 # Configure upload folder and allowed extensions
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
-application .config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_IMAGE_EXTENSIONS = { 'png', 'jpeg', 'jpg' }
+ALLOWED_VIDEO_EXTENSIONS = { 'mp4', 'avi', 'mov', 'mkv', 'webm' }
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
     
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_video_format(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_VIDEO_EXTENSIONS
+
+def allowed_image_format(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
 
 @application.route('/', methods=['GET'])
 @application.route('/health', methods=['GET'])
 def home():
     return jsonify({'message': 'Welcome to the Flask REST API!'})
-
-# @app.route('/health', methods=['GET'])
-# def echo():
-#     data = request.get_json()
-#     return jsonify({'you_sent': data}), 200
-
 
 # Endpoint to receive a video file
 @application.route('/create-video-embeddings', methods=['POST'])
@@ -43,14 +41,14 @@ def upload_video():
     if file.filename == '':
         return jsonify({'error': 'File without name'}), 400
     
-    if not file or not allowed_file(file.filename):
+    if not file or not allowed_video_format(file.filename):
         return jsonify({'error': 'Not allowed file type'}), 400
 
     filename = secure_filename(f"{post_id}_{file.filename}")
     filepath = os.path.join(application.config['UPLOAD_FOLDER'], filename)
     file.save(filepath)
 
-    query_pipe = (
+    video_pipe = (
         pipe.input('path')
             .map('path', 'frames', ops.video_decode.ffmpeg(start_time=None, end_time=None, sample_type='time_step_sample', args={'time_step': 0.5}))
             .map('frames', ('labels', 'scores', 'features'), ops.action_classification.pytorchvideo(model_name='x3d_m', skip_preprocess=True))
@@ -58,13 +56,61 @@ def upload_video():
             .output('features')
     )
 
-    result = query_pipe( os.path.abspath(filepath) )
+    result = video_pipe( os.path.abspath(filepath) )
 
     # Delete the uploaded file after processing
     os.remove(filepath)
+
     result_data = result.get()[0]
-    return jsonify({'embedding': result_data}), 200
+    return jsonify({ 'post_id': post_id, 'embedding': result_data }), 200
+
+@application.route('/create-image-embeddings', methods=['POST'])
+def create_image_embeddings():
+    post_id = request.form.get('post_id')
+    if not post_id:
+        return jsonify({'error': 'Post_id not specified'}), 400
+
+    if 'video' not in request.files:
+        return jsonify({'error': 'File no found'}), 400
+    
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({'error': 'File without name'}), 400
+    
+    if not file or not allowed_image_format(file.filename):
+        return jsonify({'error': 'Not allowed file type'}), 400
+    
+    filename = secure_filename(f"{post_id}_{file.filename}")
+
+    filepath = os.path.join(application.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    img_pipe = (
+        pipe.input('url')
+        .map('url', 'img', ops.image_decode.cv2_rgb())
+        # .map('img', 'vec', ops.image_text_embedding.clip(model_name='clip_vit_base_patch16', modality='image'))
+        .map('img', 'vec', ops.image_embedding.timm(model_name='resnet50'))
+        .output('vec')
+    )
+    result = img_pipe(filepath)
+
+    result_data = result.get()[0]
+    return jsonify({ 'post_id': post_id, 'embedding': result_data }), 200
+
+
+def test_create_image_embedding():
+    img_pipe = (
+        pipe.input('url')
+        .map('url', 'img', ops.image_decode.cv2_rgb())
+        # .map('img', 'vec', ops.image_text_embedding.clip(model_name='clip_vit_base_patch16', modality='image'))
+        .map('img', 'vec', ops.image_embedding.timm(model_name='resnet50'))
+        .output('vec')
+    )
+    result = img_pipe('./uploads/test.jpg')
+
+    result.get()[0]
 
 
 if __name__ == "__main__":
-    application .run(host='0.0.0.0', port=8025)
+    # application.run(host='0.0.0.0', port=8025)
+    application.run(host='localhost', port=5001)
